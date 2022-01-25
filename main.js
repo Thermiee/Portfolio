fb1095551962d18dfa4fecf9af77f222541bdd90
thermiee <navyblack16@yahoo.com>
2022-01-25 15:14:41 +0100

    Added eslint to linters and refactored inline javascript code

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 function openNav() {
   document.getElementById('myNav').style.display = 'block';
 }
@@ -6,3 +5,11 @@ function openNav() {
 function closeNav() {
   document.getElementById('myNav').style.display = 'none';
 }
+
+document.querySelector('.hamburger-span').addEventListener('click', openNav);
+document.querySelector('.closebtn').addEventListener('click', closeNav);
+document.querySelectorAll('.pop-nav').forEach((link) => {
+  link.addEventListener('click', () => {
+    document.getElementById('myNav').style.display = 'none';
+  });
+});
