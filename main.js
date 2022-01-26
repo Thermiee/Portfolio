2970ae05f647853f460300aaeff097b16871693b
thermiee <navyblack16@yahoo.com>
2022-01-26 15:30:37 +0100

    created details popup window with modal view

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ document.querySelectorAll('.pop-nav').forEach((link) => {
     document.getElementById('myNav').style.display = 'none';
   });
 });
+
+const modal = document.getElementById('myModal');
+const btn = document.getElementById('myBtn');
+const span = document.getElementsByClassName('close')[0];
+
+btn.onclick = () => {
+  modal.style.display = 'block';
+};
+
+span.onclick = () => {
+  modal.style.display = 'none';
+};
+
+window.onclick = (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+};
