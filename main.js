90d5325a0119fc85028d4c3da77612d39fbfc2e4
thermiee <navyblack16@yahoo.com>
2022-01-27 09:42:41 +0100

    Add contactform validation

--- main.js
+++ main.js
@@ -14,20 +14,8 @@ document.querySelectorAll('.pop-nav').forEach((link) => {
   });
 });
 
-const modal = document.getElementById('myModal');
-const btn = document.getElementById('myBtn');
-const span = document.getElementsByClassName('close')[0];
+const email = document.querySelector('#email');
+const validationAlert = document.querySelector('.alert');
+const contactForm = document.querySelector('#contact-form');
 
-btn.onclick = () => {
-  modal.style.display = 'block';
-};
 
-span.onclick = () => {
-  modal.style.display = 'none';
-};
-
-window.onclick = (event) => {
-  if (event.target === modal) {
-    modal.style.display = 'none';
-  }
-};
